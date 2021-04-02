d591da3f9c56203a163342d98f61560ea13f7657
Roman <73478501+romanbatavi@users.noreply.github.com>
2021-04-02 16:05:57 +0700

    create major header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { useTheme } from "emotion-theming";
+import PropTypes from "prop-types";
+
+// import Button from "../button/ButtonNoJSX";
+import Button from "../button/ButtonClass";
+// import Button from "../button/Button";
+import Container from "../../layout/Container";
+import Item from "../../layout/Item";
+
+// import styles from "./Header.module.css";
+import * as styles from "./Header.styles";
+
+const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
+  return (
+    <section className="header-component">
+      <Container alignItems="flex-start">
+        <Item flex={1}>
+          <Button text={showAdd ? "Done" : "Add"} onClick={showAddToggle} />
+        </Item>
+        <Item flex={2}>
+          <h1 css={styles.headerTitle(theme)}>Todo Lists</h1>
+        </Item>
+        <Item flex={1} align="right">
+          <Button text="Clear" onClick={clearTodos} color="red" align="right" />
+        </Item>
+      </Container>
+    </section>
+  );
+};
+
+Header.propTypes = {
+  showAddToggle: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired,
+  clearTodos: PropTypes.func.isRequired
+};
+
+export default Header;
---
+++ src/components/header/Header.styles.js
@@ -0,0 +1,11 @@
+import { css } from "@emotion/core";
+
+export const headerTitle = theme =>
+  css`
+    text-align: center;
+
+    color: ${theme.color.primary.black};
+    font-size: 3.6rem;
+    font-family: "Homemade Apple", sans-serif;
+    text-transform: lowercase;
+  `;
